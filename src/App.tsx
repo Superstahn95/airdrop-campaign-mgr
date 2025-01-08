047686d85dc6cccf7692a3d54daa5b3b2404875c
superstahn <superstahn@gmail.com>
2025-01-08 15:22:31 +0100

    basic app setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black text-white flex justify-center items-center">
+      My airdrop manager
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+  return <div className="flex items-center justify-between p-4">Navbar</div>;
+}
+
+export default Navbar;
